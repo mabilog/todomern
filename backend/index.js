7540d5cea8096a3bbdd02feb74c18d36a490ac3f
angelo <g.angelo.m@gmail.com>
2021-10-07 13:02:58 -0400

    todomern init

---
+++ backend/index.js
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const app = express();
+const PORT = 5000;
+app.get("/", (req, res) => {
+  res.send("Welcome to out Todo API")
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on port : ${PORT}`)
+})
